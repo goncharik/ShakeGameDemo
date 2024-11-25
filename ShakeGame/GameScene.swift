03df9614d967a67c96eae38d1e20b0183e722f1a
Eugene Honcharenko <tulusheg@gmail.com>
2024-11-25 09:39:41 +0100

    Add game logic

--- ShakeGame/GameScene.swift
+++ ShakeGame/GameScene.swift
@@ -1,88 +1,70 @@
-//
-//  GameScene.swift
-//  ShakeGame
-//
-//  Created by Eugene Honcharenko on 22/11/24.
-//
-
+import CoreMotion
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+    let motionManager = CMMotionManager()
+    var balls: [SKShapeNode] = []
+
+    override func didMove(to _: SKView) {
+        physicsWorld.gravity = CGVector(dx: 0, dy: -9.8) // Simulate gravity
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame) // Screen edges
+
+        createBalls()
+        setupDeviceMotion()
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
+
+    func createBalls() {
+        let ballRadius: CGFloat = 20.0
+
+        for _ in 0 ..< 10 {
+            let ball = SKShapeNode(circleOfRadius: ballRadius)
+            ball.fillColor = .random()
+            ball.strokeColor = .black
+            ball.lineWidth = 1
+
+            let randomX = CGFloat.random(in: ballRadius ... (size.width - ballRadius))
+            let randomY = CGFloat.random(in: (size.height / 2) ... (size.height - ballRadius))
+            ball.position = CGPoint(x: randomX, y: randomY)
+
+            ball.physicsBody = SKPhysicsBody(circleOfRadius: ballRadius)
+            ball.physicsBody?.restitution = 0.8 // Bounciness
+            ball.physicsBody?.friction = 0.1
+            ball.physicsBody?.allowsRotation = true
+            ball.physicsBody?.linearDamping = 0.1
+            ball.physicsBody?.mass = 0.5
+
+            addChild(ball)
+            balls.append(ball)
         }
     }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
+
+    func setupDeviceMotion() {
+        if motionManager.isAccelerometerAvailable {
+            motionManager.accelerometerUpdateInterval = 1.0 / 60.0 // 60 updates per second
+            motionManager.startAccelerometerUpdates(to: .main) { [weak self] data, _ in
+                guard let self, let accelerometerData = data else { return }
+                let gravity = CGVector(
+                    dx: accelerometerData.acceleration.x * 9.8,
+                    dy: accelerometerData.acceleration.y * 9.8
+                )
+                physicsWorld.gravity = gravity
+            }
         }
     }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
+
+    override func update(_: TimeInterval) {
         // Called before each frame is rendered
+        // Game loop logic can be added here if needed
+    }
+}
+
+extension UIColor {
+    static func random() -> UIColor {
+        UIColor(
+            red: .random(in: 0 ... 1),
+            green: .random(in: 0 ... 1),
+            blue: .random(in: 0 ... 1),
+            alpha: 1.0
+        )
     }
 }
--- ShakeGame/GameViewController.swift
+++ ShakeGame/GameViewController.swift
@@ -10,36 +10,14 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
-    }
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
+        let skView = SKView(frame: self.view.frame)
+        self.view.addSubview(skView)
 
-    override var prefersStatusBarHidden: Bool {
-        return true
+        let scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
     }
 }
